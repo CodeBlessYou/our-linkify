be8e942a147ce9c0ec758a8a60c335905092e61a
Code Bless You <codeblessyou24@gmail.com>
2025-06-04 13:52:42 +0530

    Initial commit

---
+++ models/chats.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+    // for groups
+    isGroup: { type: Boolean, default: true },
+    admins: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    groupName: { type: String },
+    groupImage: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.model("Chat", chatSchema);
+
+module.exports = Chat;
